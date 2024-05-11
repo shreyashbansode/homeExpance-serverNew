60708bc4493ea4b14673a8f7c10f98ee57bc736d
Shreyas Bansode <Shreyas Bansode@ho.rnt.ai>
2024-05-11 15:59:53 +0530

    Added new code

--- index.js
+++ index.js
@@ -3,19 +3,32 @@ const app = express();
 const db = require('./db')
 const cors = require('cors')
 const dotenv = require('dotenv/config');
+const Mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 5000;
+const DBurl = process.env.DBHOST;
 
-db.connection.on('connected', (err) => {
-    if (err) {
-        console.log('error while connecting database')
-    } else {
-        console.log('database is connected')
-    }
-})
+// db.connection.on('connected', (err) => {
+//     if (err) {
+//         console.log('error while connecting database')
+//     } else {
+//         console.log('database is connected')
+//     }
+// })
 app.use(express.json());
 app.use(cors())
 app.use('/', require('./routes/expanceRoute'));
+
+Mongoose.connect(DBurl)
+    .then((resp) => {
+        if (resp) {
+            console.log("Database connected.")
+        }
+        else {
+            console.log("Failed to connect to database")
+        }
+    });
+
 
 app.listen(PORT, () => {
     console.log('server is started', PORT)
