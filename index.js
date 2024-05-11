39d33c0c09d1ac9e886e3545e797a26f67d0ee7f
Shreyas Bansode <Shreyas Bansode@ho.rnt.ai>
2024-05-11 19:19:10 +0530

    Removed db code

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ const Mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
 const DBurl = process.env.DBHOST;
 
-// db.connection.on('connected', (err) => {
-//     if (err) {
-//         console.log('error while connecting database')
-//     } else {
-//         console.log('database is connected')
-//     }
-// })
 app.use(express.json());
 app.use(cors())
 app.use('/', require('./routes/expanceRoute'));
